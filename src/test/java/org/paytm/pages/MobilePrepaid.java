64dfc54f1ccfe8c46f2b84975eab380ae5f8d3cf
Sathya <sathya08ece@gmail.com>
2019-12-12 10:01:05 +0530

    test

--- src/test/java/org/paytm/pages/MobilePrepaid.java
+++ src/test/java/org/paytm/pages/MobilePrepaid.java
@@ -4,8 +4,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class MobilePrepaid {
+	
 	@FindBy(linkText="Mobile Prepaid") 
-	private WebElement Mobile_Prepaid_link;
+	WebElement Mobile_Prepaid_link;
 	@FindBy(xpath="//input[@type='tel']") WebElement MobileNumber_Prepaid;
 	@FindBy(xpath="(//input[@type='text'])[1]") WebElement Operator_Prepaid;
 	@FindBy(xpath="(//input[@type='text'])[2]") WebElement Amount_Prepaid;
@@ -13,6 +14,7 @@ public class MobilePrepaid {
 	@FindBy(linkText="Proceed to Recharge") WebElement Proceed_Prepaid;
 	@FindBy(xpath="//label[text()=\"Circle\"]") WebElement Circle_Prepaid;
 	
+
 	public WebElement getMobile_Prepaid_link() {
 		return Mobile_Prepaid_link;
 	}
@@ -40,11 +42,5 @@ public class MobilePrepaid {
 	public WebElement getCircle_Prepaid() {
 		return Circle_Prepaid;
 	}
-	
-	
-	
-	
-	
-	
 
 }
