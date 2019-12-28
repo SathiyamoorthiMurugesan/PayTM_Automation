e0a3d6cbc0a57ac203af39a27e5af108a7be1958
SathiyamoorthiMurugesan <sathya08ece@gmail.com>
2019-12-29 00:03:34 +0530

    new files

--- src/test/java/org/paytm/base/BaseClass.java
+++ src/test/java/org/paytm/base/BaseClass.java
@@ -28,7 +28,6 @@ import org.testng.annotations.*;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class BaseClass{
--- src/test/java/org/paytm/pages/MobilePostpaid.java
+++ src/test/java/org/paytm/pages/MobilePostpaid.java
@@ -1,5 +1,51 @@
 package org.paytm.pages;
 
-public class MobilePostpaid {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.paytm.base.BaseClass;
+
+public class MobilePostpaid extends BaseClass{
+	
+	@FindBy(xpath="//div[@class=\"qvWr\"]")
+	WebElement PostPaidTitle;
+
+	@FindBy(xpath="(//*[@id=\"app\"]//label)[2]")
+	WebElement PostPaidRadioButton;
+	
+	@FindBy(xpath="//*[@type=\"tel\"]")
+	WebElement PostPaidMobileNumber;
+	
+	@FindBy(xpath="(//*[@type=\"text\"])[1])")
+	WebElement PostPaidProvider;
+	
+	@FindBy(xpath="(//*[@type=\"text\"])[1]")
+	WebElement PostPaidAmount;
+	
+	@FindBy(className="_1nFl")
+	WebElement PostPaidFastForward;
+
+	public WebElement getPostPaidTitle() {
+		return PostPaidTitle;
+	}
+
+	public WebElement getPostPaidRadioButton() {
+		return PostPaidRadioButton;
+	}
+
+	public WebElement getPostPaidMobileNumber() {
+		return PostPaidMobileNumber;
+	}
+
+	public WebElement getPostPaidProvider() {
+		return PostPaidProvider;
+	}
+
+	public WebElement getPostPaidAmount() {
+		return PostPaidAmount;
+	}
+
+	public WebElement getPostPaidFastForward() {
+		return PostPaidFastForward;
+	}
 
 }
